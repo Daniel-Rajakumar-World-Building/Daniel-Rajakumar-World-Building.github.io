b562f8d36ea9ab939672eabfa5eba03526ab6a51
kingDaniel2004 <danielelvisr@gmail.com>
2022-05-20 16:31:38 -0400

    about to add sass.

--- theme.js
+++ theme.js
@@ -20,3 +20,20 @@ function toggleTheme() {
 }
 
 document.getElementById('themeButton').onclick = toggleTheme;
+
+
+console.log("hello world: " + gsap)
+let sections = gsap.utils.toArray(".panel");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: "+=3500",
+  }
+});
