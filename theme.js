a6f70ea446180438a9e71cc64a90de3f08ad2441
kingDaniel2004 <danielelvisr@gmail.com>
2022-05-22 20:14:37 -0400

    update animations.

--- theme.js
+++ theme.js
@@ -46,6 +46,6 @@ gsap.to(sections, {
 // gsap.from("#page_one_title", {
 // });
 
-gsap.fromTo("#page_one_title", 1, { x:"50vw", y:"100vh", }, { x: "50vw", y: "20vh", })
-gsap.fromTo("#page_two_title", 1, { x:"50vw", y:"100vh", }, { x: "50vw", y: "20vh", })
-gsap.fromTo("#page_three_title", 1, { x:"50vw", y:"100vh", }, { x: "50vw", y: "20vh", })
+gsap.fromTo("#page_one_title", 2, { x:"50vw", y:"100vh", }, { x: "50vw", y: "20vh", ease: "power4.out" })
+gsap.fromTo("#page_two_title", 2, { x:"50vw", y:"100vh", }, { x: "50vw", y: "20vh", ease: "power4.out"})
+gsap.fromTo("#page_three_title", 2, { x:"50vw", y:"100vh", }, { x: "50vw", y: "20vh", ease: "power4.out"})
